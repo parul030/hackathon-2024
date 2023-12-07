35a10c0d29debd6065214e2427f4e09c7b9a7471
parul030 <parulthakur030@gmail.com>
2023-12-07 21:22:21 +0530

    landing page started

---
+++ src/components/Landing/Elevate.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Elevate() {
+  return (
+    <div className="flex">
+      <div>
+        <div className=' text-left w-[441px] text-[60px] font-normal text-#2A2A3D'>Elevate your financial journey with us</div>
+        <div className=' text-left text-[18px] font-nornal text-[#5D5D70] '>Discover why FinEase is your go-to crowdlending platform</div>
+
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+   
+  );
+}
+
+export default Elevate;
