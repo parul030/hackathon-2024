da207aea41e0b9a5d7eb8507d3875c566fc39342
parul030 <parulthakur030@gmail.com>
2023-12-08 17:52:12 +0530

    feature: dashboard started

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,10 +3,13 @@ import Header from '../Common/header';
 
 function Dashboard() {
   return (
+    <div>
     <div className="relative"> 
      <div className='absolute top-0 right-0 left-0'>
       <Header />
     </div>
+    </div>
+    <div className='mt-[119px]'></div>
     </div>
   );
 }
