74f622d21dc8665a70e98422ae32eb29f36bb2e5
parul030 <parulthakur030@gmail.com>
2023-12-08 17:51:12 +0530

    feature: dashboard started

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,8 +3,10 @@ import Header from '../Common/header';
 
 function Dashboard() {
   return (
-    <div className="flex justify-between">
+    <div className="relative"> 
+     <div className='absolute top-0 right-0 left-0'>
       <Header />
+    </div>
     </div>
   );
 }
