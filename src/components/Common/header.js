cce35e4bbed572173f9042fd4b44236d0ef9b19a
parul030 <parulthakur030@gmail.com>
2023-12-08 12:00:18 +0530

    header created

---
+++ src/components/Common/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between bg-transparent'>
+        <div className='w-[201px] mt-[14px] h-[64px] ml-[70px] bg-[#2B2B40] text-[#36C3D6] text-base font-normal py-[16px] pl-[67px] text-[30px] pr-[13px]'>Fin<span className='text-[#F5BD74]'>Ease</span></div>
+        <div className='mr-[58px] mt-[35px]'>
+        <button className='text-[#36C3D6] w-[109px]  border border-[#58C3CC] bg-[#FFFFFF] text-base font-normal py-[16px] pl-[23px] pr-[26px] rounded-[10px]' variant="text">Login</button>
+        <button className='bg-[#36C3D6] text-[#FFFFFF] ml-[20px] text-base font-normal py-[16px] pl-[23px] pr-[26px] rounded-[10px]' variant="text">Sign Up</button>
+        </div>
+
+
+    </div>
+  )
+}
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from '../Common/header';
 import Elevate from './Elevate';
 import OpenAccount from './OpenAccount'
 
@@ -6,9 +7,13 @@ import OpenAccount from './OpenAccount'
 function Landing() {
   return (
     <>
-    <div className="flex relative">
-    <div className='w-[683px] h-[768px] pl-[70px]  bg-[#35354D]'>
-    <div className='w-[201px] mt-[12px] bg-[#2B2B40] text-[#36C3D6] text-base font-normal py-[16px] pl-[67px] text-[30px] pr-[13px]'>Fin<span className='text-[#F5BD74]'>Ease</span></div>
+    <div className='relative'>
+      <div className='absolute top-0 right-0 left-0'>
+      <Header/>
+      </div>
+    <div className="flex">
+    <div className='w-[50%] h-[768px] pl-[70px]  bg-[#35354D]'>
+
 
     <h1 className='text-left mt-[106px] text-[45px] font-light text-white'>Become an</h1>
     <h2 className=' text-left text-[#F5BD74] text-[100px] font-normal'>Investor</h2>
@@ -19,16 +24,11 @@ function Landing() {
     <img src = {'imgs/landing.png'} alt="lendingRIbbon"  className='w-[243px] absolute bottom-0 h-[121px]'/>
     </div>
     
-    <div className='absolute top-[40%] left-[35%]'>
+    <div className='absolute top-[40%] left-[3%]'>
     <img src = {'imgs/lenderBorrower.png'} alt="lendingRIbbon"  className=' w-[352px] h-[350px]'/>
     </div>
    
    <div className='absolute right-3 pt-[20px]'>
-    
-   <button className='text-[#36C3D6] w-[109px]  border border-[#58C3CC] bg-[#FFFFFF] text-base font-normal py-[16px] pl-[23px] pr-[26px] rounded-[10px]' variant="text">Login</button>
-
-    <button className='bg-[#36C3D6] text-[#FFFFFF] ml-[20px] text-base font-normal py-[16px] pl-[23px] pr-[26px] rounded-[10px]' variant="text">Sign Up</button>
-
     <div className='mt-[303px] mr-10'>
     <div className='mt-[106px] text-left text-[45px] font-light text-[#2A2A3D]'>Become an</div>
     <div className='text-[#F5BD74] text-left leading-normal text-[100px] font-normal'>Borrower</div>
@@ -39,6 +39,7 @@ function Landing() {
     </div>
     <Elevate/>
     <OpenAccount/>
+    </div>
     </>
   );
 }
