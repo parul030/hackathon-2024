7068d9a9fdd44f2b2e402887f37294dacfb6985c
anuj0152 <anuj0152@gmail.com>
2023-12-08 17:50:22 +0530

    folder classifications

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 import  Dashboard from './components/dashboard/Dashboard'
-import Borrower from './components/Borrower/Borrower';
+import Borrower from './components/Borrower';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/Logn';
 
 
 export default function App() {
@@ -17,7 +16,6 @@ export default function App() {
         
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="borrower" element={<Borrower />} />
-          <Route path="login" element={<Login />} />
           <Route path="/" element={<Home />}>
         
         </Route>
